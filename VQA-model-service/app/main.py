088ebd1c6b7437e4f07ae8b2855badf489994d0b
Rogelio Lozano <rogelio.aeloz@gmail.com>
2025-03-14 19:07:37 -0600

    added env variable

--- VQA-model-service/app/main.py
+++ VQA-model-service/app/main.py
@@ -4,13 +4,17 @@ from app.api.vqa import router as vqa_router
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
+import os
 
 app = FastAPI()
 
+load_dotenv()  # take environment variables from .env.
+
 origins = [
-    "http://localhost:9000",
     "http://127.0.0.1:9000",
+     os.getenv("PRODUCTION_URL"),
 ]
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,  # Allow specific origins
@@ -21,7 +25,6 @@ app.add_middleware(
 
 app.include_router(vqa_router, prefix="/vqa", tags=["Visual Question Answering"])
 
-load_dotenv()  # take environment variables from .env.
 
 # Code of your application, which uses environment variables (e.g. from `os.environ` or
 # `os.getenv`) as if they came from the actual environment.
